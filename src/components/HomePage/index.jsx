5d03bed15be19d4ed2035a6f72c524ab26de7bd4
Jason Wang <jason.wang1700@gmail.com>
2023-06-07 17:04:43 -0400

    initial commit

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import './HomePage.css'
+
+const HomePage = () => {
+
+    return (
+        <div className="navigation">
+            <p>
+                Hi, I'm Jason Wang. Let's Chat!
+            </p>
+        </div>
+    );
+};
+
+export default HomePage;
