7a6283fcfb564b47b5b137976330adb0c8464081
jwxng <jason.wang1700@gmail.com>
2023-06-08 14:50:15 -0400

    created a prototype header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-// TODO: make a global.css
-
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header'
 import HomePage from './components/HomePage'
 
 function App() {
   return (
     <div className="app">
       <Router className="router">
+        <Header />
         <Routes className="routes">
           <Route path="/" element={<HomePage />} />
         </Routes>
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h1>Jason Wang</h1>
+            <button>Experience</button>
+            <button>Projects</button>
+            <button>About Me</button>
+        </div>
+    );
+};
+
+export default Header;
